e43a423d50ed77520b10d4e69aab4f6953ef1975
MauRyze22 <amaurymperez22@gmail.com>
2026-04-06 13:28:35 -0400

    fix: Integracion de test 2

--- tasks/tests.py
+++ tasks/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth.models import User
-from .models import *
+from .models import Project, Team
 
 # Create your tests here.
 
